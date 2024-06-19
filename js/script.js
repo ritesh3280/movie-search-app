c69122db3ca976a3cf4e1023a33d2f966114fc7a
Ritesh Reddy <ritesh3280@gmail.com>
2024-06-19 01:49:40 -0700

    Added Slider

--- js/script.js
+++ js/script.js
@@ -231,6 +231,59 @@ function displayBG(type, pathBG) {
   }
 }
 
+// Display Sliding Function
+
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+
+    div.classList.add("swiper-slide");
+
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+      movie.title
+    }" />
+    </a>
+    <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i>  ${movie.vote_average.toFixed(
+          1
+        )}/ 10
+    </h4>
+    `;
+
+    document.querySelector(".swiper-wrapper").appendChild(div);
+
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidePerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Fetch data from API
 async function fetchAPIData(endpoint) {
   const API_KEY = "bd3f43f81b8610cac59244cddb991a94";
@@ -275,6 +328,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      displaySlider();
       getPopularMovies();
       break;
     case "/shows.html":
